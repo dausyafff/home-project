d66abd5f6741e1fd69997bb7bb0d25c7873c1812
Muhammad Dausyaf Aryandha <muhammaddausyaf@gmail.com>
2026-05-17 05:52:36 +0700

    api update

--- routes/api.php
+++ routes/api.php
@@ -7,14 +7,38 @@ use App\Http\Controllers\Api\SkillController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
-//public Routes
+// public Routes
 Route::post("/register", [AuthController::class, "register"]);
 Route::post("/login", [AuthController::class, "login"]);
 
-Route::apiResource("posts", PostController::class);
-Route::apiResource("skills", SkillController::class);
-Route::apiResource("projects", ProjectController::class);
+// public GET
+Route::get('/projects',      [ProjectController::class, 'index']);
+Route::get('/projects/{project}', [ProjectController::class, 'show']);
+Route::get('/skills',        [SkillController::class, 'index']);
+Route::get('/skills/{skill}',     [SkillController::class, 'show']);
+Route::get('/posts',         [PostController::class, 'index']);
+Route::get('/posts/{post}',       [PostController::class, 'show']);
+
+// protected Routes (need token)
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/me',      [AuthController::class, 'me']);
+
+    // Hanya authenticated user yang bisa CUD
+    Route::post('/projects',              [ProjectController::class, 'store']);
+    Route::put('/projects/{project}',     [ProjectController::class, 'update']);
+    Route::delete('/projects/{project}',  [ProjectController::class, 'destroy']);
+
+    Route::post('/skills',                [SkillController::class, 'store']);
+    Route::put('/skills/{skill}',         [SkillController::class, 'update']);
+    Route::delete('/skills/{skill}',      [SkillController::class, 'destroy']);
+
+    Route::post('/posts',                 [PostController::class, 'store']);
+    Route::put('/posts/{post}',           [PostController::class, 'update']);
+    Route::delete('/posts/{post}',        [PostController::class, 'destroy']);
+});
