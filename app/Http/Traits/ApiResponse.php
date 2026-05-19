44685142f3dda43436dd658f9e0f51e60a86b5bc
Muhammad Dausyaf Aryandha <muhammaddausyaf@gmail.com>
2026-05-19 10:46:05 +0700

    base response trait

---
+++ app/Http/Traits/ApiResponse.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Traits;
+
+trait ApiResponse
+{
+    protected function success($data = null, $message = 'Success', $code = 200)
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => $message,
+            'data' => $data,
+        ], $code);
+    }
+    protected function error($message = 'Error', $code = 400, $data = null)
+    {
+        return response()->json([
+            'status' => 'error',
+            'message' => $message,
+            'data' => $data,
+        ], $code);
+    }
+}
