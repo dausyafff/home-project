f850a34abe3667c101385d72ca418ecb40b867f0
Muhammad Dausyaf Aryandha <muhammaddausyaf@gmail.com>
2026-08-26 05:11:28 +0700

    migration update

--- config/services.php
+++ config/services.php
@@ -30,7 +30,7 @@ return [
 
     'slack' => [
         'notifications' => [
-            'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
+            'botc_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
--- database/migrations/2026_08_24_225155_add_google_id_to_users_table.php
+++ database/migrations/2026_08_24_225155_add_google_id_to_users_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string("google_id")->nullable()->after("id");
+            $table->string("avatar")->nullable()->after("google_id");
+            $table->string("password")->nullable()->change();
         });
     }
 
@@ -22,7 +24,8 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn(["google_id", "avatar"]);
+            $table->string("password")->nullable(false)->change();
         });
     }
 };
